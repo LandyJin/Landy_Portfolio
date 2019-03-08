f5d58af36d740cbb305e854f14307b8903a74c2e
JIN HUIYAN <huiyanj91@gmail.com>
2019-03-09 01:01:53 +1100

    build the app

---
+++ public/precache-manifest.b8d6cddec0c663778dfef61cabec8bb1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "31e0c047f20b4e7dc0da",
+    "url": "/static/css/main.c89fc40f.chunk.css"
+  },
+  {
+    "revision": "31e0c047f20b4e7dc0da",
+    "url": "/static/js/main.1ceb29ba.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a92c6ef7ad1b3d492453",
+    "url": "/static/css/2.549f27cb.chunk.css"
+  },
+  {
+    "revision": "a92c6ef7ad1b3d492453",
+    "url": "/static/js/2.e49386d0.chunk.js"
+  },
+  {
+    "revision": "7b2ac97c32986e4507f05c2980f42262",
+    "url": "/static/media/index1.7b2ac97c.mp4"
+  },
+  {
+    "revision": "8261c672541e0ce2758d4337ce17d72a",
+    "url": "/static/media/index.8261c672.jpeg"
+  },
+  {
+    "revision": "5c81c20182b0a9a75c4ec84dc0329fcb",
+    "url": "/static/media/moreAboutMe.5c81c201.png"
+  },
+  {
+    "revision": "ce2a2aa5f5e3d11f05950072ad44eeea",
+    "url": "/static/media/backgroundImg.ce2a2aa5.png"
+  },
+  {
+    "revision": "1e2bf53104dec90a0c45aa8e081cd506",
+    "url": "/static/media/profile.1e2bf531.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "70cde634612a245c2ee5d7a466b7051f",
+    "url": "/index.html"
+  }
+];

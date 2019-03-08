f5907321a3dca1005b17ed38671f171b9f383187
JIN HUIYAN <huiyanj91@gmail.com>
2019-03-09 00:51:04 +1100

    build the app

--- index.js
+++ index.js
@@ -27,17 +27,32 @@ app.post('/api/form', (req, res) => {
         `
 
         // create reusable transporter object using the default SMTP transport
-        
-
         let transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for 465, false for other ports
+            server: "gmail",
             auth: {
-              user: "rosalind38@ethereal.email", // generated ethereal user
-              pass: "N6u6rwgqgw5zWkEdc3" // generated ethereal password
-            }
-          });        
+                type: 'OAuth2',
+                    user: 'huiyanj91@gmail.com',
+                    pass: 'taoyanni1991',
+                    clientId: '891350879165-hhki7nprsm0ev2c0hcd1o8ojg0ikblji.apps.googleusercontent.com',
+                    clientSecret: 'niFPcXHtZk6bjDAzpwjkwq_o',
+                    refreshToken: '1/z9A5vdVB3ZND4bAprq_DAPMW4iPlA13hrjdXjiJmznWZK7UxTp97I_1JP2bT7aPu',
+                    // accessToken: 'ya29.GlvGBrMEU1K8rdKXzwBP-fSHGwW2TaZt5zVNT1bfR2wGeFa2Uj3nLFjeaevon7Ym4EQHCIJhKNIezCqwlhrZFr7c68ndRB-eg00Tii0AHP0597gOWZamzcEISA-Z'
+                
+            },
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for  other ports
+        });
+
+        // let transporter = nodemailer.createTransport({
+        //     host: "smtp.ethereal.email",
+        //     port: 587,
+        //     secure: false, // true for 465, false for other ports
+        //     auth: {
+        //       user: "rosalind38@ethereal.email", // generated ethereal user
+        //       pass: "N6u6rwgqgw5zWkEdc3" // generated ethereal password
+        //     }
+        //   });        
 
         // setup email data with unicode symbols
         let mailOptions = {
